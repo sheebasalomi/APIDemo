b78cc68c86e567053c6988302c610575ad3b5bf0
sheebasalomi11@gmail.com <sheebasalomi11@gmail.com>
2021-07-20 11:16:51 -0500

    initial

---
+++ SlnEmployee/WebApiEmployee/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiEmployee.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SlnEmployee/WebApiEmployee/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiEmployee.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
