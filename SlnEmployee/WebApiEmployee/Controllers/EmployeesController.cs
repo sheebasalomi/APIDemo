2abd18624dad2983d9160b0d1b34a20bf8323ddd
sheebasalomi11@gmail.com <sheebasalomi11@gmail.com>
2021-07-21 12:54:35 -0500

    implementing delete in Employee Controller

--- SlnEmployee/WebApiEmployee/Controllers/EmployeesController.cs
+++ SlnEmployee/WebApiEmployee/Controllers/EmployeesController.cs
@@ -50,6 +50,31 @@ namespace WebApiEmployee.Controllers
             }
         }
 
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity != null)
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with the id " + id + " not found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
 
     }
 }
