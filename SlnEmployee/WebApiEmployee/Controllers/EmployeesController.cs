100e35bf82435d38014a5e25c50fbce774c7c428
sheebasalomi11@gmail.com <sheebasalomi11@gmail.com>
2021-07-21 11:29:01 -0500

    Entityframework added

---
+++ SlnEmployee/WebApiEmployee/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebApiEmployee.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            EmployeeDBEntities entities = new EmployeeDBEntities();
+            return entities.Employees;
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+    }
+}
